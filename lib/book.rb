a3dbfda60a78b39666550c69ae4edbd26e17444f
Trevor Vardeman <trevortx@gmail.com>
2022-06-17 00:22:17 -0700

    commit tree c05e09dba3960de009eb9c57572a6689b1cd6506

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize title="And Then There Were None"
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition, :cobble
+
+  def initialize brand="Adidas"
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
